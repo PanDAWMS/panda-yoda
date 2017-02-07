f47f5de7b8f61b9a35bdaaa9abac3f0dd8df4758
jtchilders <taylor@jtchilders.com>
2017-02-06 21:09:50 -0600

    update indentation to 3 spaces, update yoda logging.

--- pandayoda/yodacore/Interaction.py
+++ pandayoda/yodacore/Interaction.py
@@ -14,251 +14,251 @@ sendQueue = Queue.Queue()
 # class to receive requests
 class Receiver:
 
-    # constructor
-    def __init__(self, rank=None, nonMPIMode=False, logger=None):
-        if nonMPIMode:
-            self.comm = None
-            self.stat = None
-            self.nRank = 0
-            self.totalRanks = 1
-            self.selectSource = None
-        else:
-            from mpi4py import MPI
-            self.comm = MPI.COMM_WORLD
-            self.stat = MPI.Status()
-            self.nRank = self.comm.Get_rank()
-            self.totalRanks = self.comm.Get_size()
-            self.selectSource = MPI.ANY_SOURCE
+   # constructor
+   def __init__(self, rank=None, nonMPIMode=False, logger=None):
+      if nonMPIMode:
+         self.comm = None
+         self.stat = None
+         self.nRank = 0
+         self.totalRanks = 1
+         self.selectSource = None
+      else:
+         from mpi4py import MPI
+         self.comm = MPI.COMM_WORLD
+         self.stat = MPI.Status()
+         self.nRank = self.comm.Get_rank()
+         self.totalRanks = self.comm.Get_size()
+         self.selectSource = MPI.ANY_SOURCE
 
-        self.logger = logger
+      self.logger = logger
 
-        # for message in rank 0
-        self.hasMessage = False
-        self.recvQueue = recvQueue
-        self.sendQueue = sendQueue
-
-
-    # get rank of itself
-    def getRank(self):
-        return self.comm.Get_rank() if self.comm else self.nRank
+      # for message in rank 0
+      self.hasMessage = False
+      self.recvQueue = recvQueue
+      self.sendQueue = sendQueue
 
 
-    # receive request
-    def receiveRequest(self):
-        # wait for a request from any ranks
-        t1 = time.time()
-        self.hasMessage = False
-        if self.comm:
-            while not self.comm.Iprobe(source=self.selectSource,
-                                       status=self.stat):
-                if not self.recvQueue.empty():
-                    self.hasMessage = True
-                    break
-                time.sleep(0.0001)
-                if (time.time() - t1) > 40 * 60:
-                    # waiting too log, should quit.
-                    errMsg = 'No messages received for 40 minutes. quit'
-                    return False,errMsg,None
-        else:
-            while self.recvQueue.empty():
-                time.sleep(0.0001)
-                if (time.time() - t1) > 40 * 60:
-                    # waiting too log, should quit.
-                    errMsg = 'No messages received for 40 minutes. quit'
-                    return False,errMsg,None
-            self.hasMessage = True
+   # get rank of itself
+   def getRank(self):
+      return self.comm.Get_rank() if self.comm else self.nRank
 
-        if self.hasMessage:
-            try:
-                reqData = self.recvQueue.get()
-                # self.stat.Set_source(0)
-                data = json.loads(reqData)
-                return True,data['method'],data['params']
-            except:
-                errtype,errvalue = sys.exc_info()[:2]
-                errMsg = 'failed to got proper request with: %s' % traceback.format_exc()
-                return False,errMsg,None
-        else:
-            try:
-                # get the request
-                # reqData = self.comm.recv(source=self.selectSource, status=self.stat)
-                reqData = self.comm.recv(source=self.stat.Get_source())
-                # decode
-                data = json.loads(reqData)
-                return True,data['method'],data['params']
-            except:
-                errtype,errvalue = sys.exc_info()[:2]
-                errMsg = 'failed to got proper request with: %s' % traceback.format_exc()
-                return False,errMsg,None
+
+   # receive request
+   def receiveRequest(self):
+      # wait for a request from any ranks
+      t1 = time.time()
+      self.hasMessage = False
+      if self.comm:
+         while not self.comm.Iprobe(source=self.selectSource,
+                                    status=self.stat):
+            if not self.recvQueue.empty():
+               self.hasMessage = True
+               break
+            time.sleep(0.0001)
+            if (time.time() - t1) > 40 * 60:
+               # waiting too log, should quit.
+               errMsg = 'No messages received for 40 minutes. quit'
+               return False,errMsg,None
+      else:
+         while self.recvQueue.empty():
+            time.sleep(0.0001)
+            if (time.time() - t1) > 40 * 60:
+               # waiting too log, should quit.
+               errMsg = 'No messages received for 40 minutes. quit'
+               return False,errMsg,None
+         self.hasMessage = True
+
+      if self.hasMessage:
+         try:
+            reqData = self.recvQueue.get()
+            # self.stat.Set_source(0)
+            data = json.loads(reqData)
+            return True,data['method'],data['params']
+         except:
+            errtype,errvalue = sys.exc_info()[:2]
+            errMsg = 'failed to got proper request with: %s' % traceback.format_exc()
+            return False,errMsg,None
+      else:
+         try:
+            # get the request
+            # reqData = self.comm.recv(source=self.selectSource, status=self.stat)
+            reqData = self.comm.recv(source=self.stat.Get_source())
+            # decode
+            data = json.loads(reqData)
+            return True,data['method'],data['params']
+         except:
+            errtype,errvalue = sys.exc_info()[:2]
+            errMsg = 'failed to got proper request with: %s' % traceback.format_exc()
+            return False,errMsg,None
 
 
     # return response 
     def returnResponse(self,rData):
-        if self.hasMessage:
-            try:
-                #data = urllib.urlencode(rData)
-                data = json.dumps(rData)
-                self.sendQueue.put(data)
-                return True,None
-            except:
-                errtype,errvalue = sys.exc_info()[:2]
-                errMsg = 'failed to retrun response with: %s' % traceback.format_exc()
-                return False,errMsg,None
-        else:
-            try:
-                #data = urllib.urlencode(rData)
-                data = json.dumps(rData)
-                self.comm.send(data,dest=self.stat.Get_source())
-                return True,None
-            except:
-                errtype,errvalue = sys.exc_info()[:2]
-                errMsg = 'failed to retrun response with: %s' % traceback.format_exc()
-                return False,errMsg,None
-
-
-    # get rank of the requester
-    def getRequesterRank(self):
-        return self.stat.Get_source() if self.stat else self.nRank
-
-        
-    # decrement nRank
-    def decrementNumRank(self):
-        self.totalRanks -= 1
-        
-
-    # check if there is active worker rank
-    def activeRanks(self):
-        return self.totalRanks > 0
-
-
-    def getTotalRanks(self):
-        return self.totalRanks
-
-
-    def sendMessage(self, rData):
-        try:
+      if self.hasMessage:
+         try:
             #data = urllib.urlencode(rData)
             data = json.dumps(rData)
-            for i in range(1, self.totalRanks):
-                self.comm.send(data,dest=i)
             self.sendQueue.put(data)
             return True,None
-        except:
+         except:
+            errtype,errvalue = sys.exc_info()[:2]
+            errMsg = 'failed to retrun response with: %s' % traceback.format_exc()
+            return False,errMsg,None
+      else:
+         try:
+            #data = urllib.urlencode(rData)
+            data = json.dumps(rData)
+            self.comm.send(data,dest=self.stat.Get_source())
+            return True,None
+         except:
             errtype,errvalue = sys.exc_info()[:2]
             errMsg = 'failed to retrun response with: %s' % traceback.format_exc()
             return False,errMsg,None
 
 
-    def disconnect(self):
-        try:
-            self.comm.Disconnect()
-            return True,None
-        except:
-            errtype,errvalue = sys.exc_info()[:2]
-            errMsg = 'failed to disconnect with: %s' % traceback.format_exc()
-            return False,errMsg
+   # get rank of the requester
+   def getRequesterRank(self):
+      return self.stat.Get_source() if self.stat else self.nRank
+
+        
+   # decrement nRank
+   def decrementNumRank(self):
+      self.totalRanks -= 1
+        
+
+   # check if there is active worker rank
+   def activeRanks(self):
+      return self.totalRanks > 0
+
+
+   def getTotalRanks(self):
+      return self.totalRanks
+
+
+   def sendMessage(self, rData):
+      try:
+         #data = urllib.urlencode(rData)
+         data = json.dumps(rData)
+         for i in range(1, self.totalRanks):
+             self.comm.send(data,dest=i)
+         self.sendQueue.put(data)
+         return True,None
+      except:
+         errtype,errvalue = sys.exc_info()[:2]
+         errMsg = 'failed to retrun response with: %s' % traceback.format_exc()
+         return False,errMsg,None
+
+
+   def disconnect(self):
+      try:
+         self.comm.Disconnect()
+         return True,None
+      except:
+         errtype,errvalue = sys.exc_info()[:2]
+         errMsg = 'failed to disconnect with: %s' % traceback.format_exc()
+         return False,errMsg
 
 
 # class to send requests
 class Requester:
     
-    # constructor
-    def __init__(self, rank=None, nonMPIMode=False, logger=None):
-        self.nonMPIMode = nonMPIMode
-        if not self.nonMPIMode:
-            from mpi4py import MPI
-            self.comm = MPI.COMM_WORLD
-            self.rank = 0
-        else:
-            self.comm = None
+   # constructor
+   def __init__(self, rank=None, nonMPIMode=False, logger=None):
+     self.nonMPIMode = nonMPIMode
+      if not self.nonMPIMode:
+         from mpi4py import MPI
+         self.comm = MPI.COMM_WORLD
+         self.rank = 0
+      else:
+         self.comm = None
 
-        self.logger = logger
+      self.logger = logger
 
-        # for message in rank 0
-        self.hasMessage = False
-        self.recvQueue = sendQueue
-        self.sendQueue = recvQueue
+      # for message in rank 0
+      self.hasMessage = False
+      self.recvQueue = sendQueue
+      self.sendQueue = recvQueue
 
-    # get rank of itself
-    def getRank(self):
-        if self.nonMPIMode:
-            return 0
-        return self.comm.Get_rank()
+   # get rank of itself
+   def getRank(self):
+      if self.nonMPIMode:
+         return 0
+      return self.comm.Get_rank()
 
 
-    # send request
-    def sendRequest(self,method,params):
-        try:
-            # encode
-            data = {'method':method,
-                    'params':params}
-            reqData = json.dumps(data)
+   # send request
+   def sendRequest(self,method,params):
+      try:
+         # encode
+         data = {'method':method,
+                 'params':params}
+         reqData = json.dumps(data)
+         if self.getRank() == 0:
+             self.sendQueue.put(reqData)
+         else:
+             # send a request ro rank0
+             self.comm.send(reqData,dest=0)
+
+         while True:
             if self.getRank() == 0:
-                self.sendQueue.put(reqData)
+               ansData = self.recvQueue.get(True, timeout=1000)
             else:
-                # send a request ro rank0
-                self.comm.send(reqData,dest=0)
+               # wait for the answer from Rank 0
+               # while not self.comm.Iprobe(source=0):
+               #    time.sleep(0.001)
+               # get the answer
+               ansData = self.comm.recv(source=0)
+            # decode
+            #answer = cgi.parse_qs(ansData)
+            answer = json.loads(ansData)
 
-            while True:
-                if self.getRank() == 0:
-                    ansData = self.recvQueue.get(True, timeout=1000)
-                else:
-                    # wait for the answer from Rank 0
-                    # while not self.comm.Iprobe(source=0):
-                    #    time.sleep(0.001)
-                    # get the answer
-                    ansData = self.comm.recv(source=0)
-                # decode
-                #answer = cgi.parse_qs(ansData)
-                answer = json.loads(ansData)
-
-                # special handler for signal
-                # {'StatusCode':0, 'State': 'signal', 'signum': signum}
-                try:
-                    if 'StatusCode' in answer and answer['StatusCode'] == 0 and 'State' in answer and  answer['State'] == 'signal':
-                        if self.logger:
-                            self.logger.debug("Received signal messages: %s" % answer)
-                        os.kill(os.getpid(), answer['signum'])
-                        continue
-                    else:
-                        break
-                except:
-                    if self.logger:
-                        self.logger.debug("Failed to handle signal message: %s" % traceback.format_exc())
-                    break
-            return True,answer
+            # special handler for signal
+            # {'StatusCode':0, 'State': 'signal', 'signum': signum}
+            try:
+               if 'StatusCode' in answer and answer['StatusCode'] == 0 and 'State' in answer and  answer['State'] == 'signal':
+                  if self.logger:
+                     self.logger.debug("Received signal messages: %s" % answer)
+                  os.kill(os.getpid(), answer['signum'])
+                  continue
+               else:
+                  break
+            except:
+               if self.logger:
+                  self.logger.debug("Failed to handle signal message: %s" % traceback.format_exc())
+               break
+         return True,answer
         except:
             errtype,errvalue = sys.exc_info()[:2]
             errMsg = 'failed to send the request with mode %s: %s' % (self.nonMPIMode, traceback.format_exc())
             return False,errMsg
             
         
-    def waitMessage(self):
-        try:
-            if self.getRank() == 0:
-                ansData = self.recvQueue.get(True, timeout=0.0001)
-            else:
-                # wait for message from Rank 0
-                # if self.comm.Iprobe(source=0):
-                #    time.sleep(0.0001)
-                # get the answer
-                ansData = self.comm.Irecv(source=0)
-            # decode
-            #answer = cgi.parse_qs(ansData)
-            answer = json.loads(ansData)
-            return True,answer
-        except:
-            errtype,errvalue = sys.exc_info()[:2]
-            errMsg = 'failed to receive msg with: %s' % traceback.format_exc()
-            return False,errMsg
+   def waitMessage(self):
+      try:
+         if self.getRank() == 0:
+            ansData = self.recvQueue.get(True, timeout=0.0001)
+         else:
+            # wait for message from Rank 0
+            # if self.comm.Iprobe(source=0):
+            #    time.sleep(0.0001)
+            # get the answer
+            ansData = self.comm.Irecv(source=0)
+         # decode
+         #answer = cgi.parse_qs(ansData)
+         answer = json.loads(ansData)
+         return True,answer
+      except:
+         errtype,errvalue = sys.exc_info()[:2]
+         errMsg = 'failed to receive msg with: %s' % traceback.format_exc()
+         return False,errMsg
 
 
-    def disconnect(self):
-        try:
-            if self.comm:
-                self.comm.Disconnect()
-            return True,None
-        except:
-            errtype,errvalue = sys.exc_info()[:2]
-            errMsg = 'failed to disconnect with: %s' % traceback.format_exc()
-            return False,errMsg
+   def disconnect(self):
+      try:
+         if self.comm:
+            self.comm.Disconnect()
+         return True,None
+      except:
+         errtype,errvalue = sys.exc_info()[:2]
+         errMsg = 'failed to disconnect with: %s' % traceback.format_exc()
+         return False,errMsg
--- pandayoda/yodaexe/yoda-droid.py
+++ pandayoda/yodaexe/yoda-droid.py
@@ -6,125 +6,118 @@ import sys
 import time
 import traceback
 from mpi4py import MPI
+from pandayoda.yodacore import Yoda
+from pandayoda.yodaexe import Droid
 logger = logging.getLogger(__name__)
 
-def main(globalWorkDir, localWorkDir, nonMPIMode=False, outputDir=None, dumpEventOutputs=True):
-    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+def main(globalWorkDir, localWorkDir, outputDir=None, dumpEventOutputs=True):
 
-    if nonMPIMode:
-        comm = None
-        mpirank = 0
-        mpisize = 1
-    else:
-        try:
-            comm = MPI.COMM_WORLD
-            mpirank = comm.Get_rank()
-            mpisize = comm.Get_size()
-        except:
-            logger.exception("Failed to load mpi4py")
-            raise
+   # get MPI world info
+   comm = MPI.COMM_WORLD
+   mpirank = comm.Get_rank()
+   mpisize = comm.Get_size()
 
-    # Create separate working directory for each rank
-    from os.path import abspath as _abspath, join as _join
-    curdir = _abspath (localWorkDir)
-    wkdirname = "rank_%s" % str(mpirank)
-    wkdir  = _abspath (_join(curdir,wkdirname))
-    if not os.path.exists(wkdir):
-        os.makedirs (wkdir)
-    os.chdir (wkdir)
+   # Create separate working directory for each rank
+   curdir = os.path.abspath (localWorkDir)
+   wkdirname = "rank_%08i" % mpirank
+   wkdir  = os.path.abspath (os.path.join(curdir,wkdirname))
+   if not os.path.exists(wkdir):
+      os.makedirs (wkdir)
+   os.chdir (wkdir)
 
-    logger.info("GlobalWorkDir: %s" % globalWorkDir)
-    logger.info("LocalWorkDir: %s" % localWorkDir)
-    logger.info("OutputDir: %s" % outputDir)
-    logger.info("RANK: %s" % mpirank)
+   logger.info("GlobalWorkDir: %s" % globalWorkDir)
+   logger.info("LocalWorkDir: %s" % localWorkDir)
+   logger.info("OutputDir: %s" % outputDir)
+   logger.info("RANK: %08i of %08i" % (mpirank,mpisize))
 
-    if mpirank==0:
-        try:
-            from pandayoda.yodacore import Yoda
-            yoda = Yoda.Yoda(globalWorkDir, localWorkDir, rank=0, nonMPIMode=nonMPIMode, outputDir=outputDir, dumpEventOutputs=dumpEventOutputs)
-            yoda.start()
+   if mpirank==0:
+     try:
+         yoda = Yoda.Yoda(globalWorkDir, localWorkDir, rank=0, nonMPIMode=nonMPIMode, 
+                           outputDir=outputDir, dumpEventOutputs=dumpEventOutputs)
+         yoda.start()
 
-            from pandayoda.yodaexe import Droid
-            if nonMPIMode:
-                reserveCores = 0
+         
+         if nonMPIMode:
+            reserveCores = 0
+         else:
+            reserveCores = 1
+         droid = Droid.Droid(globalWorkDir, localWorkDir, rank=0, nonMPIMode=True, 
+                             reserveCores=reserveCores, outputDir=outputDir)
+         droid.start()
+
+         i = 30
+         while True:
+            logger.info("Rank %s: Yoda isAlive %s" % (mpirank, yoda.isAlive()))
+            logger.info("Rank %s: Droid isAlive %s" % (mpirank, droid.isAlive()))
+
+            if yoda and yoda.isAlive():
+              time.sleep(60)
             else:
-                reserveCores = 1
-            droid = Droid.Droid(globalWorkDir, localWorkDir, rank=0, nonMPIMode=True, reserveCores=reserveCores, outputDir=outputDir)
-            droid.start()
-
-            i = 30
-            while True:
-                logger.info("Rank %s: Yoda isAlive %s" % (mpirank, yoda.isAlive()))
-                logger.info("Rank %s: Droid isAlive %s" % (mpirank, droid.isAlive()))
-
-                if yoda and yoda.isAlive():
-                    time.sleep(60)
-                else:
-                    break
-            logger.info("Rank %s: Yoda finished" % (mpirank))
-        except:
-            logger.exception("Rank %s: Yoda failed" % mpirank)
-            raise
-        #os._exit(0)
-    else:
-        try:
-            status = 0
-            from pandayoda.yodaexe import Droid
-            droid = Droid.Droid(globalWorkDir, localWorkDir, rank=mpirank, nonMPIMode=nonMPIMode, outputDir=outputDir)
-            droid.start()
-            while (droid and droid.isAlive()):
-                droid.join(timeout=1)
-            # parent process
-            #pid, status = os.waitpid(child_pid, 0)
-            logger.info("Rank %s: Droid finished status: %s" % (mpirank, status))
-        except:
-            logger.exception("Rank %s: Droid failed" % mpirank)
-            raise
-    return mpirank
+               break
+         logger.info("Rank %s: Yoda finished" % (mpirank))
+      except:
+         logger.exception("Rank %s: Yoda failed" % mpirank)
+         raise
+     #os._exit(0)
+   else:
+      try:
+         status = 0
+         droid = Droid.Droid(globalWorkDir, localWorkDir, rank=mpirank, 
+                             nonMPIMode=nonMPIMode, outputDir=outputDir)
+         droid.start()
+         while (droid and droid.isAlive()):
+             droid.join(timeout=1)
+         # parent process
+         #pid, status = os.waitpid(child_pid, 0)
+         logger.info("Rank %s: Droid finished status: %s" % (mpirank, status))
+      except:
+         logger.exception("Rank %s: Droid failed" % mpirank)
+         raise
+   return mpirank
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,format='%(asctime)s|%(process)s|%(levelname)s|%(name)s|%(message)s',datefmt='%Y-%m-%d %H:%M:%S')
-    usage = """
-usage: %(prog)s <command> [options] [args]
+   logging.basicConfig(level=logging.INFO,
+         format='%(asctime)s|%(process)s|%(levelname)s|%(name)s|%(message)s',
+         datefmt='%Y-%m-%d %H:%M:%S')
 
-Commands:
+   oparser = argparse.ArgumentParser()
+   oparser.add_argument('--globalWorkingDir',dest="globalWorkingDir",
+                        help="Global share working directory",required=True)
+   oparser.add_argument('--localWorkingDir',dest="localWorkingDir",
+                        help="Local working directory. if it's not set, it will use global working directory",
+                        required=True)
+   oparser.add_argument('--outputDir', dest="outputDir",help="Copy output files to this directory")
+   oparser.add_argument('--dumpEventOutputs',default=False,action='store_true',
+                        help="Dump event output info to xml")
+   oparser.add_argument('--verbose', '-v', default=False, action='store_true',
+                        help="Set logger to DEBUG.")
+   oparser.add_argument('--quiet', '-q', default=False, action='store_true',
+                        help="Set logger to ERROR.")
+   args = oparser.parse_args()
+   
+   if args.verbose and not args.quiet:
+      logger.setLevel(logging.DEBUG)
+   elif not args.verbose and args.quiet:
+      logger.setLevel(logging.ERROR)
+   elif not args.verbose and not args.quiet:
+      logger.setLevel(logging.INFO)
+   else:
+      logger.error(' Cannot set both verbose and quiet flag. ')
+      oparser.print_help()
+      sys.exit(-1)
 
-    help <command>  Output help for one of the commands below
-
-"""
-    oparser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]), add_help=True)
-    oparser.add_argument('--globalWorkingDir', dest="globalWorkingDir", default=None, help="Global share working directory")
-    oparser.add_argument('--localWorkingDir', dest="localWorkingDir", default=None, help="Local working directory. if it's not set, it will use global working directory")
-    oparser.add_argument('--nonMPIMode', default=False, action='store_true', help="Run Yoda in non-MPI mode")
-    oparser.add_argument('--outputDir', dest="outputDir", default=None, help="Copy output files to this directory")
-    oparser.add_argument('--dumpEventOutputs', default=False, action='store_true', help="Dump event output info to xml")
-    oparser.add_argument('--verbose', '-v', default=False, action='store_true', help="Print more verbose output.")
-
-    if len(sys.argv) == 1:
-        oparser.print_help()
-        sys.exit(-1)
-
-    args = oparser.parse_args(sys.argv[1:])
-    if args.globalWorkingDir is None:
-        logger.error( "Global working directory is needed.")
-        oparser.print_help()
-        sys.exit(-1)
-    if args.localWorkingDir is None:
-        args.localWorkingDir = args.globalWorkingDir
-
-    rank = None
-    try:
-        logger.info("Start HPCJob")
-        rank = main(args.globalWorkingDir, args.localWorkingDir, args.nonMPIMode, args.outputDir, args.dumpEventOutputs)
-        logger.info( "Rank %s: HPCJob-Yoda success" % rank )
-        if rank == 0:
-            # this causes all MPI ranks to exit, uncleanly
-            MPI.COMM_WORLD.Abort(-1)
-            sys.exit(0)
-    except Exception as e:
-        logger.exception("Rank " + rank + ": HPCJob-Yoda failed, exiting all ranks.")
-        if rank == 0:
-            # this causes all MPI ranks to exit, uncleanly
-            MPI.COMM_WORLD.Abort(-1)
-            sys.exit(-1)
-    #os._exit(0)
+   try:
+     logger.info("Start HPCJob")
+     rank = main(args.globalWorkingDir, args.localWorkingDir, args.nonMPIMode, args.outputDir, args.dumpEventOutputs)
+     logger.info( "Rank %s: HPCJob-Yoda success" % rank )
+     if rank == 0:
+         # this causes all MPI ranks to exit, uncleanly
+         MPI.COMM_WORLD.Abort(-1)
+         sys.exit(0)
+   except Exception as e:
+     logger.exception("Rank " + rank + ": HPCJob-Yoda failed, exiting all ranks.")
+     if rank == 0:
+         # this causes all MPI ranks to exit, uncleanly
+         MPI.COMM_WORLD.Abort(-1)
+         sys.exit(-1)
+   #os._exit(0)
