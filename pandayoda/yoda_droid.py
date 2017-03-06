cd75d82ebaee784f37847f6e47c960bf0386f530
Taylor Childers <jchilders@anl.gov>
2017-03-06 06:31:09 -0800

    changed to using a config file to setup yoda

--- pandayoda/yoda_droid.py
+++ pandayoda/yoda_droid.py
@@ -12,19 +12,57 @@ from pandayoda.droid import Droid
 logger = logging.getLogger(__name__)
 
 
-def yoda_droid(globalWorkDir, localWorkDir, outputDir=None, dumpEventOutputs=True):
+def yoda_droid(configFile='yoda.cfg'):
+
+   # config defaults
+   defaults={
+      'DumpEventOutputs':'false',
+      'LogLevel':'INFO',
+      'LocalWorkingDir':None,
+      'OutputDir':None,
+      'InputJobFile':'HPCJobs.json',
+   }
+
+   # parse config file
+   logger.info("Reading Configuration from file: %s",configFile)
+   if os.path.exists(configFile):
+      config = ConfigParser.RawConfigParser(defaults)
+      config.read(configFile)
+   else:
+      raise Exception(" input configuration file does not exist ")
+
+   logLevel = config.get('yoda','LogLevel')
+   if logLevel == 'INFO':
+      logger.setLevel(logging.INFO)
+   elif logLevel == 'DEBUG':
+      logger.setLevel(logging.DEBUG)
+   elif logLevel == 'WARNING':
+      logger.setLevel(logging.WARNING)
+   elif logLevel == 'ERROR':
+      logger.setLevel(logging.ERROR)
+   elif logLevel == 'CRITICAL':
+      logger.setLevel(logging.CRITICAL)
+   else:
+      raise Exception(' LogLevel set to %s which is not accepted',logLevel)
 
 
-   logger.info("GlobalWorkDir:    %s",globalWorkDir)
-   logger.info("LocalWorkDir:     %s",localWorkDir)
+   try:
+      globalWorkingDir     = config.get('yoda','GlobalWorkingDir')
+   except:
+      logger.error(' must specify GlobalWorkingDir in the config file %s',configFile)
+      raise
+   localWorkingDir      = config.get('yoda','LocalWorkingDir')
+   if localWorkingDir is None:
+      localWorkingDir = globalWorkingDir
+   outputDir         = config.get('yoda','OutputDir')
+   dumpEventOutputs  = config.getboolean('yoda','DumpEventOutputs')
+
+   logger.info("GlobalWorkingDir:    %s",globalWorkingDir)
+   logger.info("LocalWorkingDir:     %s",localWorkingDir)
    logger.info("OutputDir:        %s",outputDir)
    logger.info("DumpEventOutputs: %s",str(dumpEventOutputs))
 
-   # parse config file
-   configFile = os.path.join(os.path.realpath(__file__),'yoda.cfg')
-   config = ConfigParser.RawConfigParser()
-   config.read(configFile)
-
+   
    inputJobFile = config.get('yoda','InputJobFile')
    logger.info("InputJobFile:  %s",inputJobFile)
 
@@ -34,12 +72,13 @@ def yoda_droid(globalWorkDir, localWorkDir, outputDir=None, dumpEventOutputs=Tru
       comm = MPI.COMM_WORLD
       mpirank = comm.Get_rank()
       mpisize = comm.Get_size()
+      logger.debug(' rank %10i of %10i',mpirank,mpisize)
    except:
       logger.error('Exception retrieving MPI rank information')
       raise
 
    # Create separate working directory for each rank
-   curdir = os.path.abspath(localWorkDir)
+   curdir = os.path.abspath(localWorkingDir)
    wkdirname = "rank_%08i" % mpirank
    wkdir  = os.path.abspath(os.path.join(curdir,wkdirname))
    if not os.path.exists(wkdir):
@@ -50,12 +89,12 @@ def yoda_droid(globalWorkDir, localWorkDir, outputDir=None, dumpEventOutputs=Tru
 
    if mpirank==0:
       try:
-         yoda = Yoda.Yoda(globalWorkDir, localWorkDir, 
+         yoda = Yoda.Yoda(globalWorkingDir, localWorkingDir, 
                            outputDir=outputDir, dumpEventOutputs=dumpEventOutputs,
                            inputJobFile=inputJobFile)
          yoda.start()
          
-         droid = Droid.Droid(globalWorkDir, localWorkDir, outputDir=outputDir)
+         droid = Droid.Droid(globalWorkingDir, localWorkingDir, outputDir=outputDir)
          droid.start()
 
          while yoda.isAlive() or droid.isAlive():
@@ -71,7 +110,7 @@ def yoda_droid(globalWorkDir, localWorkDir, outputDir=None, dumpEventOutputs=Tru
    else:
       try:
          status = 0
-         droid = Droid.Droid(globalWorkDir, localWorkDir, outputDir=outputDir)
+         droid = Droid.Droid(globalWorkingDir, localWorkingDir, outputDir=outputDir)
          droid.start()
          
          while droid.isAlive():
@@ -85,37 +124,12 @@ def main():
    logging.basicConfig(level=logging.INFO,
          format='%(asctime)s|%(process)s|%(levelname)s|%(name)s|%(message)s',
          datefmt='%Y-%m-%d %H:%M:%S')
-
+   logging.info('Start yoda_droid')
    oparser = argparse.ArgumentParser()
-   oparser.add_argument('--globalWorkingDir',dest="globalWorkingDir",
-                        help="Global share working directory",required=True)
-   oparser.add_argument('--localWorkingDir',dest="localWorkingDir",
-                        help="Local working directory. if it's not set, it will use global working directory",default=None)
-   oparser.add_argument('--outputDir', dest="outputDir",
-                        help="Copy output files to this directory",default=None)
-   oparser.add_argument('--dumpEventOutputs',default=False,action='store_true',
-                        help="Dump event output info to xml")
-   oparser.add_argument('--verbose', '-v', default=False, action='store_true',
-                        help="Set logger to DEBUG.")
-   oparser.add_argument('--quiet', '-q', default=False, action='store_true',
-                        help="Set logger to ERROR.")
+   oparser.add_argument('-c','--config',dest='configFile',help='input configuration filename',default='yoda.cfg')
    args = oparser.parse_args()
-   
-   if args.verbose and not args.quiet:
-      logger.setLevel(logging.DEBUG)
-   elif not args.verbose and args.quiet:
-      logger.setLevel(logging.ERROR)
-   elif not args.verbose and not args.quiet:
-      logger.setLevel(logging.INFO)
-   else:
-      logger.error(' Cannot set both verbose and quiet flag. ')
-      oparser.print_help()
-      sys.exit(-1)
 
-   if args.localWorkingDir == None:
-      args.localWorkingDir = args.globalWorkingDir
-
-   yoda_droid(args.globalWorkingDir, args.localWorkingDir, args.outputDir, args.dumpEventOutputs)
+   yoda_droid(args.configFile)
 
 
 if __name__ == "__main__":
