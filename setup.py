4e81a4e685c4d77f70c525aee24fe560a0a36ab0
palnilsson <Paul.Nilsson@cern.ch>
2019-05-10 15:32:15 +0200

    Flake8 corrections

--- setup.py
+++ setup.py
@@ -26,7 +26,8 @@ setup(
                },
    data_files=[
       ('etc/panda', ['pandayoda/yoda_template.cfg']),
-      ('templates', ['templates/ThetaSubmitTF.sh', 'templates/CoriHaswellSubmitTF.sh']),
+      ('templates', ['templates/ThetaSubmitTF.sh',
+                     'templates/CoriHaswellSubmitTF.sh']),
    ],
    classifiers=[
       'Development Status :: 3 - Alpha',
