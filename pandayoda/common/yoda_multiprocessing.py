55da220c5a9976772f11d30c714dd6033953d9f2
palnilsson <Paul.Nilsson@cern.ch>
2019-05-21 10:32:28 +0200

    Flake8 corrections

--- pandayoda/common/yoda_multiprocessing.py
+++ pandayoda/common/yoda_multiprocessing.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 # http://www.apache.org/licenses/LICENSE-2.0
 #
 # - Taylor Childers (john.taylor.childers@cern.ch)
+# - Paul Nilsson (paul.nilsson@cern.ch)
 
 import os
-from multiprocessing import *
 
 # running inside the Athena release, at least for 21.0.15,
 # the python version can be strange. While it reports Python 2.7
--- pandayoda/version.py
+++ pandayoda/version.py
@@ -1,1 +1,9 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Taylor Childers (john.taylor.childers@cern.ch)
+
 version = "0.8-1-g643a7da"
